f0f30fab49cfdfce68e363b5297c163b15525c4f
maxliu2001 <yinghao_liu@yahoo.com>
2023-06-22 14:08:27 -0400

    init watchOS app

---
+++ MiNap Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MiNap Watch App
+//
+//  Created by Yinghao Liu on 6/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
