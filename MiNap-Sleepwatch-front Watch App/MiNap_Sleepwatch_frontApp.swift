f5cc454701b60ad7819b974ea2c97223e91cfdb6
Quishi Zhao <91136802+qiushizUM@users.noreply.github.com>
2023-07-11 16:12:35 -0400

    push all files from Xcode

---
+++ MiNap-Sleepwatch-front Watch App/MiNap_Sleepwatch_frontApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiNap_Sleepwatch_frontApp.swift
+//  MiNap-Sleepwatch-front Watch App
+//
+//  Created by Quishi Zhao on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct MiNap_Sleepwatch_front_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# MiNap-front-end
+
+## Version 1.0.0
+## Actions this week
+* Create a sleep button screen (done)
+* Create a wake-up button screen (done)
+* Create animation/image screen (to do)
