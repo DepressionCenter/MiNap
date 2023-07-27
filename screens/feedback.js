faf3156051bf7c0133ea9b62e7e2a5a5d3368199
Anika Raisa Chowdhury <anikaraisa1995@gmail.com>
2023-07-27 10:26:19 -0400

    Skip button and mandatory fields

--- screens/feedback.js
+++ screens/feedback.js
@@ -10,11 +10,26 @@ const FeedbackScreen = () => {
   const [additionalFeedback, setAdditionalFeedback] = useState('');
 
   const submitFeedback = () => {
+    if (sleepQuality === '' || interruption === '' || caffeineConsumed === '' || alcoholConsumed === '') {
+      alert('Please answer all mandatory questions.');
+      return;
+    }
+
     console.log('Sleep Quality:', sleepQuality);
     console.log('Interruption:', interruption);
     console.log('Caffeine Consumed:', caffeineConsumed);
     console.log('Alcohol Consumed:', alcoholConsumed);
     console.log('Additional Feedback:', additionalFeedback);
+
+    setSleepQuality('');
+    setInterruption('');
+    setCaffeineConsumed('');
+    setAlcoholConsumed('');
+    setAdditionalFeedback('');
+  };
+
+  const skipFeedback = () => {
+    console.log('User chose to skip the feedback.');
 
     setSleepQuality('');
     setInterruption('');
@@ -29,7 +44,10 @@ const FeedbackScreen = () => {
         <Text style={styles.header}>Feedback Form</Text>
 
         {/* Question 1 */}
-        <Text style={styles.question}>Question 1. How well did you sleep?</Text>
+        <Text style={styles.question}>
+          Question 1* How well did you sleep?
+          <Text style={styles.redAsterisk}>*</Text>
+        </Text>
         <RadioButton.Group onValueChange={value => setSleepQuality(value)} value={sleepQuality}>
           <RadioButton.Item label="Good" value="good" />
           <RadioButton.Item label="Average" value="average" />
@@ -37,21 +55,30 @@ const FeedbackScreen = () => {
         </RadioButton.Group>
 
         {/* Question 2 */}
-        <Text style={styles.question}>Question 2. Did you face any interruption in between the sleep session? Did you wake up in between?</Text>
+        <Text style={styles.question}>
+          Question 2* Did you face any interruption in between the sleep session? Did you wake up in between?
+          <Text style={styles.redAsterisk}>*</Text>
+        </Text>
         <RadioButton.Group onValueChange={value => setInterruption(value)} value={interruption}>
           <RadioButton.Item label="Yes" value="yes" />
           <RadioButton.Item label="No" value="no" />
         </RadioButton.Group>
 
         {/* Question 3 */}
-        <Text style={styles.question}>Question 3. Did you consume caffeine prior to sleeping?</Text>
+        <Text style={styles.question}>
+          Question 3* Did you consume caffeine prior to sleeping?
+          <Text style={styles.redAsterisk}>*</Text>
+        </Text>
         <RadioButton.Group onValueChange={value => setCaffeineConsumed(value)} value={caffeineConsumed}>
           <RadioButton.Item label="Yes" value="yes" />
           <RadioButton.Item label="No" value="no" />
         </RadioButton.Group>
 
         {/* Question 4 */}
-        <Text style={styles.question}>Question 4. Did you consume alcohol before sleeping?</Text>
+        <Text style={styles.question}>
+          Question 4* Did you consume alcohol before sleeping?
+          <Text style={styles.redAsterisk}>*</Text>
+        </Text>
         <RadioButton.Group onValueChange={value => setAlcoholConsumed(value)} value={alcoholConsumed}>
           <RadioButton.Item label="Yes" value="yes" />
           <RadioButton.Item label="No" value="no" />
@@ -65,10 +92,15 @@ const FeedbackScreen = () => {
           onChangeText={text => setAdditionalFeedback(text)}
         />
 
-        {/* Submit Button */}
-        <Button mode="contained" onPress={submitFeedback}>
-          Submit
-        </Button>
+        {/* Submit and Skip Buttons */}
+        <View style={styles.buttonsContainer}>
+          <Button mode="contained" onPress={submitFeedback}>
+            Submit
+          </Button>
+          <Button mode="outlined" onPress={skipFeedback}>
+            Skip
+          </Button>
+        </View>
       </Surface>
     </View>
   );
@@ -98,6 +130,15 @@ const styles = StyleSheet.create({
   question: {
     fontSize: 16,
     marginBottom: 8,
+  },
+//   for madatory questions
+  redAsterisk: {
+    color: 'red',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 16,
   },
 });
 
